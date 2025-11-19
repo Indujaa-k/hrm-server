b04ebaa627e3dd008208b93ff16d0e65019c2999
Indujaa-k <indujaakalichami3366@gmail.com>
2025-11-19 18:08:46 +0530

    auths change

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,72 +1,153 @@
+// const tokenService = require('../services/token-service');
+// const userService = require('../services/user-service');
+// const ErrorHandler = require('../utils/error-handler');
+// const {TokenExpiredError} = require('jsonwebtoken');
+
+// const auth = async (req,res,next) =>
+// {
+//     const {accessToken:accessTokenFromCookie,refreshToken:refreshTokenFromCookie}  = req.cookies;
+//     try{
+//         if(!accessTokenFromCookie)
+//             return next(ErrorHandler.unAuthorized())
+//         const userData = await tokenService.verifyAccessToken(accessTokenFromCookie);
+//         if(!userData)
+//             throw new Error(ErrorHandler.unAuthorized());
+//         req.user= userData;
+//     }
+//     catch(e)
+//     {
+//         console.log('Token Error');
+//         if(e instanceof TokenExpiredError)
+//         {
+//             console.log('Trying To Generate New Token');
+//             if(!refreshTokenFromCookie) return next(ErrorHandler.unAuthorized());
+//                 const userData = await tokenService.verifyRefreshToken(refreshTokenFromCookie);
+//                 const {_id,email,username,type} = userData;
+//                 const token = await tokenService.findRefreshToken(_id,refreshTokenFromCookie);
+//                 if(!token) return next(ErrorHandler.unAuthorized());
+//                 const payload = {
+//                     _id,
+//                     email,
+//                     username,
+//                     type
+//                 }
+//                 const {accessToken,refreshToken} = tokenService.generateToken(payload);
+//                 await tokenService.updateRefreshToken(_id,refreshTokenFromCookie,refreshToken);
+//                 const user = await userService.findUser({email});
+//                 if(user.status!='active') return next(ErrorHandler.unAuthorized('There is a problem with your account, Please contact to the admin'));
+//                 req.user = user;
+//                 req.cookies.accessToken = accessToken;
+//                 req.cookies.refreshToken = refreshToken;
+//                 res.cookie('accessToken',accessToken,{
+//                     maxAge:1000*60*60*24*30,
+//                     // httpOnly:true
+//                 })
+//                 res.cookie('refreshToken',refreshToken,{
+//                     maxAge:1000*60*60*24*30,
+//                     // httpOnly:true
+//                 })
+//                 console.log('Token Generated Success');
+//                     return next();
+//             }
+//         else
+//             return next(ErrorHandler.unAuthorized());
+//     }
+//     next();
+// }
+
+// const authRole = (role) =>
+// {
+//     return (req,res,next)=>
+//     {
+//         if(!role.includes(req.user.type))
+//             return next(ErrorHandler.notAllowed());
+//         next();
+//     }
+// }
+
+
+// module.exports ={
+//     auth,
+//     authRole
+// }
+
+
 const tokenService = require('../services/token-service');
 const userService = require('../services/user-service');
 const ErrorHandler = require('../utils/error-handler');
-const {TokenExpiredError} = require('jsonwebtoken');
+const { TokenExpiredError } = require('jsonwebtoken');
 
-const auth = async (req,res,next) =>
-{
-    const {accessToken:accessTokenFromCookie,refreshToken:refreshTokenFromCookie}  = req.cookies;
-    try{
-        if(!accessTokenFromCookie)
-            return next(ErrorHandler.unAuthorized())
+const auth = async (req, res, next) => {
+    const { accessToken: accessTokenFromCookie, refreshToken: refreshTokenFromCookie } = req.cookies;
+
+    try {
+        if (!accessTokenFromCookie) return next(ErrorHandler.unAuthorized());
+
         const userData = await tokenService.verifyAccessToken(accessTokenFromCookie);
-        if(!userData)
-            throw new Error(ErrorHandler.unAuthorized());
-        req.user= userData;
-    }
-    catch(e)
-    {
-        console.log('Token Error');
-        if(e instanceof TokenExpiredError)
-        {
-            console.log('Trying To Generate New Token');
-            if(!refreshTokenFromCookie) return next(ErrorHandler.unAuthorized());
-                const userData = await tokenService.verifyRefreshToken(refreshTokenFromCookie);
-                const {_id,email,username,type} = userData;
-                const token = await tokenService.findRefreshToken(_id,refreshTokenFromCookie);
-                if(!token) return next(ErrorHandler.unAuthorized());
-                const payload = {
-                    _id,
-                    email,
-                    username,
-                    type
-                }
-                const {accessToken,refreshToken} = tokenService.generateToken(payload);
-                await tokenService.updateRefreshToken(_id,refreshTokenFromCookie,refreshToken);
-                const user = await userService.findUser({email});
-                if(user.status!='active') return next(ErrorHandler.unAuthorized('There is a problem with your account, Please contact to the admin'));
-                req.user = user;
-                req.cookies.accessToken = accessToken;
-                req.cookies.refreshToken = refreshToken;
-                res.cookie('accessToken',accessToken,{
-                    maxAge:1000*60*60*24*30,
-                    // httpOnly:true
-                })
-                res.cookie('refreshToken',refreshToken,{
-                    maxAge:1000*60*60*24*30,
-                    // httpOnly:true
-                })
-                console.log('Token Generated Success');
-                    return next();
+        if (!userData) throw new Error(ErrorHandler.unAuthorized());
+
+        req.user = userData;
+        return next();
+    } catch (e) {
+        console.log('Token Error:', e.message);
+
+        // If token expired, try to refresh
+        if (e instanceof TokenExpiredError) {
+            if (!refreshTokenFromCookie) return next(ErrorHandler.unAuthorized());
+
+            const userData = await tokenService.verifyRefreshToken(refreshTokenFromCookie);
+            const { _id, email, username, type } = userData;
+
+            const tokenExists = await tokenService.findRefreshToken(_id, refreshTokenFromCookie);
+            if (!tokenExists) return next(ErrorHandler.unAuthorized());
+
+            const payload = { _id, email, username, type };
+            const { accessToken, refreshToken } = tokenService.generateToken(payload);
+
+            // Update refresh token in DB
+            await tokenService.updateRefreshToken(_id, refreshTokenFromCookie, refreshToken);
+
+            // Check user status
+            const user = await userService.findUser({ email });
+            if (user.status !== 'active') {
+                return next(ErrorHandler.unAuthorized('There is a problem with your account. Please contact admin.'));
             }
-        else
+
+            // Set new cookies for frontend
+            res.cookie('accessToken', accessToken, {
+                maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+                httpOnly: true,    // cannot be accessed by JS
+                secure: true,      // HTTPS only
+                sameSite: 'none'   // allow cross-origin (Vercel <-> Render)
+            });
+
+            res.cookie('refreshToken', refreshToken, {
+                maxAge: 1000 * 60 * 60 * 24 * 30,
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            });
+
+            req.user = user;
+            console.log('Token refreshed successfully.');
+            return next();
+        } else {
             return next(ErrorHandler.unAuthorized());
+        }
     }
-    next();
-}
+};
 
-const authRole = (role) =>
-{
-    return (req,res,next)=>
-    {
-        if(!role.includes(req.user.type))
+// Role-based access middleware
+const authRole = (role) => {
+    return (req, res, next) => {
+        if (!role.includes(req.user.type)) {
             return next(ErrorHandler.notAllowed());
+        }
         next();
-    }
-}
+    };
+};
 
-
-module.exports ={
+module.exports = {
     auth,
     authRole
-}
+};
